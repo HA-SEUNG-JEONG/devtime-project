27c6a027864d19763837fc28872f04c009c46b9e
Jeong Ha Seung <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2025-11-10 18:13:25 +0900

    🔧 import 순서 변경

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import './index.css';
 import './styles/colors.css';
 import './styles/font.css';
-import './index.css';
 import App from './App.tsx';
 
 createRoot(document.getElementById('root')!).render(
