5bdbfa3cbc54cc9f4da62017c98dc30dfeefaa0b
Jeong Ha Seung <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2025-12-25 00:37:12 +0900

    ✨ textarea 컴포넌트 추가

---
+++ src/component/TextAreaComponent.stories.tsx
@@ -0,0 +1,19 @@
+import type { Meta, StoryObj } from "@storybook/react-vite";
+import TextAreaComponent from "./TextAreaComponent";
+
+const meta = {
+  title: "TextAreaComponent",
+  component: TextAreaComponent,
+  parameters: {
+    layout: "centered"
+  },
+  tags: ["autodocs"]
+} satisfies Meta<typeof TextAreaComponent>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {}
+};
---
+++ src/component/TextAreaComponent.tsx
@@ -0,0 +1,8 @@
+import { Textarea } from "@/components/ui/textarea";
+import React from "react";
+
+const TextAreaComponent = () => {
+  return <Textarea placeholder="Type your message here." />;
+};
+
+export default TextAreaComponent;
