bd891b782f5f269e1f225299802967cc38bd5675
Jeong Ha Seung <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2025-12-26 10:16:14 +0900

    🔧 side 및 delayDuration을 자유롭게 사용할 수 있도록 리팩토링

--- src/component/TooltipComponent.stories.tsx
+++ src/component/TooltipComponent.stories.tsx
@@ -5,15 +5,56 @@ const meta = {
   title: "TooltipComponent",
   component: TooltipComponent,
   parameters: {
-    layout: "centered"
+    layout: "centered",
   },
-  tags: ["autodocs"]
+  tags: ["autodocs"],
 } satisfies Meta<typeof TooltipComponent>;
 
 export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {
-  args: {}
+export const DefaultSideBottom: Story = {
+  args: {
+    content: "Tooltip Content",
+    children: "Hover",
+    side: "bottom",
+    delayDuration: 0,
+  },
+};
+
+export const DefaultSideTop: Story = {
+  args: {
+    content: "Tooltip Content",
+    children: "Hover",
+    side: "top",
+    delayDuration: 0,
+  },
+};
+
+export const DefaultSideLeft: Story = {
+  args: {
+    content: "Tooltip Content",
+    children: "Hover",
+    side: "left",
+    delayDuration: 0,
+  },
+};
+
+export const DefaultSideRight: Story = {
+  args: {
+    content: "Tooltip Content",
+    children: "Hover",
+    side: "right",
+    delayDuration: 0,
+  },
+};
+
+export const DefaultDelayDuration1000: Story = {
+  args: {
+    content: "Tooltip Content",
+    children: "Hover",
+    side: "bottom",
+    delayDuration: 1000,
+  },
 };
--- src/component/TooltipComponent.tsx
+++ src/component/TooltipComponent.tsx
@@ -4,15 +4,29 @@ import {
   TooltipContent,
 } from "@/components/ui/tooltip";
 
-const TooltipComponent = () => {
+interface TooltipComponentProps {
+  content: React.ReactNode;
+  children: React.ReactNode;
+  side?: "top" | "bottom" | "left" | "right";
+  delayDuration?: number;
+}
+
+const TooltipComponent = ({
+  content,
+  children,
+  side,
+  delayDuration,
+}: TooltipComponentProps) => {
   return (
-    <Tooltip delayDuration={0}>
+    <Tooltip delayDuration={delayDuration}>
       <TooltipTrigger>
         <button className="typography-body-b bg-primary-0 cursor-pointer rounded-[5px] px-4 py-3 text-white">
-          Hover
+          {children}
         </button>
       </TooltipTrigger>
-      <TooltipContent side="bottom">Hover</TooltipContent>
+      <TooltipContent sideOffset={8} side={side}>
+        {content}
+      </TooltipContent>
     </Tooltip>
   );
 };
